005da19b7423204f1ec7c228a67b390e27113761
evarkitecta <evarkitecta@gmail.com>
2023-09-08 22:46:49 +0200

    Pruebas maquetación contenido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './layout/pages/main-page/main-page.component';
 import { HomePageComponent } from './starwars/pages/home/home-page.component';
+import { StarshipsPageComponent } from './starwars/pages/starships-page/starships-page.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,11 @@ const routes: Routes = [
         // Ya no se carga el componente, se carga el modulo mediante un dynamic import de js nativo
         // loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
       },
+      {
+        path: 'starships',
+        component: StarshipsPageComponent,
+      },
+
       {
         path: '**',
         redirectTo: 'home',
--- src/app/starwars/starwars.module.ts
+++ src/app/starwars/starwars.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { StarshipsPageComponent } from './pages/starships-page/starships-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    StarshipsPageComponent
   ],
   imports: [
     CommonModule
